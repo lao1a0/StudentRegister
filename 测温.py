c600190272c58bf06672b8d765ea86e179d5d23f
laolao <46106062+thinkforanameissohard@users.noreply.github.com>
2020-08-17 00:08:21 +0800

    Update 测温.py

--- 测温.py
+++ 测温.py
@@ -9,22 +9,26 @@ from 语音合成 import *
 
 
 def thermometry():
+    '''
+        返回0代表温度异常
+    '''
     dis_cnt = 0
     sensor = MLX90614()
 
     while (1):
-        distance = distance()
+        dist = distance()
         # 判定距离是否在合适拍照的范围
-        if distance > 10:
+        print("［debug］当前距离为{:.2f}cm ".format(dist))
+        if dist <= 10:
             dis_cnt += 1
         else:
             dis_cnt = 0
-            print("请靠近")
+            print("\t\t请靠近")
         # 连续检测到三次符合距离要求，跳出循环准备开始人脸检测
         if (dis_cnt == 3):
             break
-        # 每隔0.5秒钟进行一次测距
-        time.sleep(0.5)
+        # 每隔1秒钟进行一次测距
+        time.sleep(1)
 
     temperature = ""
     for j in range(7):
@@ -42,8 +46,13 @@ def thermometry():
         # 温度异常
         speak = "温度异常" + str(temperature)
         tag =0
-    Baidu_Speak(speak)
+    #Baidu_Speak(speak)
+    print(speak)
     return  tag
 
 if __name__ == '__main__':
-    thermometry()
+    try:
+        thermometry()
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
