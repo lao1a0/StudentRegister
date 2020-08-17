eb6949c9257785933e4b03adbade1d18cf793f75
laolao <46106062+thinkforanameissohard@users.noreply.github.com>
2020-08-17 14:36:25 +0800

    Create 数据库数据.sql

---
+++ 测试数据/数据库数据.sql
@@ -0,0 +1,14 @@
+create database test;
+create table user(
+`学号` varchar(50) primary key,
+`姓名` varchar(50),
+`班级` varchar(50),
+`宿舍` varchar(50),
+`学院` varchar(50),
+`辅导员` varchar(50)
+);
+insert into user values('20183614','卢修斯·马尔福','信1803','207','斯莱特林学院','萨拉查·斯莱特林');
+insert into user values('20183613','西弗勒斯·斯内普','信1603','407','斯莱特林学院','萨拉查·斯莱特林');
+insert into user values('20183013','哈利·波特','信1823','247','格兰芬多学院','戈德里克·格兰芬多');
+insert into user values('20183513','赫敏·格兰杰','信1823','247','格兰芬多学院','戈德里克·格兰芬多');
+insert into user values('20183523','罗恩·韦斯莱','信1823','247','格兰芬多学院','戈德里克·格兰芬多');
